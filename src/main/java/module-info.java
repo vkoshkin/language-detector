25728e8a1ee6cbf868c2a155842935bfe53fec99
vkoshkin <mail@vkoshkin.com>
2025-01-09 16:53:57 +0300

    Add additional opens

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,9 @@ module com.optimaize.langdetector {
     exports com.optimaize.langdetect.i18n;
     exports com.optimaize.langdetect.ngram;
     exports com.optimaize.langdetect.profiles;
+    exports com.optimaize.langdetect.profiles.util;
     exports com.optimaize.langdetect.text;
+
+    opens languages;
+    opens languages.shorttext;
 }
