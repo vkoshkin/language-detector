e7b0b950ec0c40c5e2d14ed8ea51d1fb8aafa529
vkoshkin <mail@vkoshkin.com>
2025-01-09 16:31:53 +0300

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.optimaize.langdetector {
+    requires org.jetbrains.annotations;
+
+    exports com.optimaize.langdetect;
+    exports com.optimaize.langdetect.cybozu.util;
+    exports com.optimaize.langdetect.frma;
+    exports com.optimaize.langdetect.i18n;
+    exports com.optimaize.langdetect.ngram;
+    exports com.optimaize.langdetect.profiles;
+    exports com.optimaize.langdetect.text;
+}
