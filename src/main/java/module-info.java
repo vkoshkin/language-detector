6409900079df86b55dbda29de7376ae355215480
vkoshkin <mail@vkoshkin.com>
2025-01-09 22:25:58 +0300

    Remove redundant exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,10 @@ module com.optimaize.langdetector {
     requires org.jetbrains.annotations;
 
     exports com.optimaize.langdetect;
-    exports com.optimaize.langdetect.cybozu.util;
     exports com.optimaize.langdetect.frma;
     exports com.optimaize.langdetect.i18n;
     exports com.optimaize.langdetect.ngram;
     exports com.optimaize.langdetect.profiles;
-    exports com.optimaize.langdetect.profiles.util;
     exports com.optimaize.langdetect.text;
 
     opens languages;
